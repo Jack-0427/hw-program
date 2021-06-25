704bee3383f669d6c4317056c3ff48e626303758
Jack-0427 <80628339+Jack-0427@users.noreply.github.com>
2021-06-26 01:12:30 +0800

    Update rail75.c

--- rail75.c
+++ rail75.c
@@ -1,138 +1,112 @@
 #include <stdlib.h>
 #include <string.h>
-#include <iostream> 
-#include <time.h> 
-using namespace std; 
+#include <stdio.h>
 
-struct Contact{
-	char first[10];
-	char last[10];
-	int relation;
-	char phone[10];
+struct node{
+	int data;
+	struct node* next;
 };
 
-int compare_relation(int n, int m){
-	if(n == m)
-		return 0;	
-	else if(n < m)
-		return -1;
-	else if(n > m)
-		return 1;
+typedef struct node Node;
+
+Node* TOP[1000] = {NULL};
+
+Node* add(Node* top, int n){
+	Node* newer = (Node*)malloc(sizeof(Node));
+	newer->next = top;
+	newer->data = n;
+	top = newer;
+	return top;
 }
 
-int compare_string(char* ch1, char* ch2){
-	if(*ch1 == *ch2 && *ch1 != '\0')
-		return compare_string(ch1+1, ch2+1);
-	else if(*ch1 == *ch2)
-		return 0;
-	else if(*ch1 < *ch2)
-		return -1;
-	else if(*ch1 > *ch2)
-		return 1;
-}
-
-int compare_phone(char* p, char* q){
-int i = 0;
-while(*p++ == *q++ && i++ < 10)
-	if(i == 10)
-		return 0;
-	else if(*p < *q)
-		return -1;
-	else if(*p > *q)
-		return 1;
-}
-
-int compare(Contact* p1, Contact* p2)
-{
-	int d = compare_relation(p1->relation, p2->relation);
-	int e = compare_string(p1->first, p2->first);
-	int f = compare_string(p1->last, p2->last);
-	int g = compare_phone(p1->phone, p2->phone);
-	if(d == -1)
-		return -1;
-	else if(d == 1)
-		return 1;
-	else{
-		if(e == -1)
-			return -1;
-		else if(e == 1)
-			return 1;
-		else{
-			if(f == -1)
-				return -1;
-			else if(f == 1)
-				return 1;
-			else{
-				if(g == -1)
-					return -1;
-				else if(g == 1)
-					return 1;
-				else
-					return 0;
-			}
-}
-}
-}
-
-int getStringlen(char* p, char* q){
-	int i = 0;
-	while(*p++ != '\0')
-		i++;
-	while(*q++ != '\0')
-		i++;
-	return i;
-}
-
-void insertion_sort(Contact* base, int n, size_t size, int (*compar)(Contact*, Contact*)){
-	int i = 0, j, a;
-	Contact* tmp[n];
-	Contact* temp;
-	Contact p[n];
-	for(int i = 0; i < n; i++){
-		tmp[i] = (Contact*)malloc(sizeof(Contact));
-		tmp[i] = base+i; 
+Node* deleted(Node* top){
+	if(!top){
+		return NULL;
 	}
-	for(i = 0; i < n; i++){
-		j = i;
-		if(!temp)	temp = (Contact*)malloc(sizeof(Contact));
-		while(j > 0 && compare(tmp[j], tmp[j-1]) == -1){
-			temp = tmp[j];
-			tmp[j] = tmp[j-1];
-			tmp[j-1] = temp;
-			j--;
+	Node* tmp = top;
+	top = top->next;
+	free(tmp);
+	tmp = NULL;
+	return top;
+}
+
+Node* migrate(Node* close, Node* open){
+	if(close != NULL){
+		Node* p = close;
+		Node* q = close;
+		if(!close->next){
+			if(open == NULL){
+				return close;
+			}
+			else{
+				close->next = open;
+				return close;
+			}
+		}
+		close = close->next;
+		p->next = open;
+		while(close->next){
+			p = close;
+			close = close->next;
+			p->next = q;
+			q = p;
+		}
+		close->next = p;
+		return close;
+	}
+	else{
+		return open;
+	}
+}
+
+void print(Node *head){
+	if (head != NULL){
+		print(head ->next);
+		printf("%d ", head->data);
+		free(head);
+		head = NULL;
+	}
+}
+
+int main(){
+	int rail, line;
+	scanf("%d %d", &rail, &line);
+	int i = 1, m = -1, n = -1;
+	char instruction[22];
+	char entering[6] = "enter";
+	char leaf[6] = "leave";
+	char migration[8] = "migrate";
+	
+	for(i = 1 ; i <= line; i++)
+	{
+		scanf("%s", instruction);
+		if(instruction[0] == 'm'){
+			scanf("%d", &m);
+			scanf("%d", &n);
+			if(n < rail && m < rail){
+				TOP[n] = migrate(TOP[m], TOP[n]);
+				TOP[m] = NULL;
+			}
+		}
+		
+		else if(instruction[0] == 'l'){
+			scanf("%d", &m);
+			if(m < rail){
+				TOP[m] = deleted(TOP[m]);
+			}
+		}
+		
+		else if(instruction[0] == 'e'){
+			scanf("%d", &m);
+			scanf("%d", &n);
+			if(m < rail){
+				TOP[m] = add(TOP[m], n);
+			}
 		}
 	}
-	for(i = 0; i < n; i++){
-		strcpy(p[i].first, tmp[i]->first);
-		strcpy(p[i].last, tmp[i]->last);
-		strcpy(p[i].phone, tmp[i]->phone);
-		p[i].relation = tmp[i]->relation;
+	for(i = 0; i < rail; i++){
+		print(TOP[i]);
+		printf("\n");
 	}
-	for(i = 0; i < n; i++){
-		strcpy(base[i].first, p[i].first);
-		strcpy(base[i].last, p[i].last);
-		strcpy(base[i].phone, p[i].phone);
-		base[i].relation = p[i].relation;
-	}	
+	return 0;
 }
-
-Contact contact[50000];
-int main(){
-	double START,END;
-	START = clock();
-	FILE *infile;
-	int blank[1000];
-	infile = fopen("C:\\46.txt", "r");
-	for(int i = 0; i < 10000; i++){
-		fscanf(infile, "%d %s %s %s\n", &contact[i].relation, contact[i].first, contact[i].last, contact[i].phone);
-	}
-	insertion_sort(contact, 10000, sizeof(Contact), compare);
-//	for(int i = 0; i < 10000; i++){
-//		printf("%d %s %s %s\n", contact[i].relation, contact[i].first, contact[i].last, contact[i].phone);
-//	}
-	END = clock();
-	cout << endl << "程式執行所花費：" << (double)clock()/CLOCKS_PER_SEC << " S";
-	cout << endl << "進行運算所花費的時間：" << (END - START) / CLOCKS_PER_SEC << " S" << endl;
-	fclose(infile);
-}
-
