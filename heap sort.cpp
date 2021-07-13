39f5c9de59f7e98e95794e6cfcfa4bc2d23b7b4c
Jack-0427 <80628339+Jack-0427@users.noreply.github.com>
2021-07-13 11:47:09 +0800

    Add files via upload

---
+++ heap sort.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void swap(int* x, int* y){
+	*x = *x + *y;
+	*y = *x - *y;
+	*x = *x - *y;
+}
+
+int* heapsort(int* A, int n){
+	int* sort = (int*)malloc(sizeof(int)*n);
+	int x = 0;
+	while(n > 0){
+		int i = n-1, min = 0;
+		while(i > 0){
+			int j = (i-1) / 2;
+			if(A[i] < A[j]){
+				swap(A+i, A+j);
+				j = i;
+				while(j*2 < n){
+					if((j+1)*2 == n){
+						min = j*2+1;
+					}
+					else if((j+1)*2 < n){
+						min = (A[j*2+1] < A[(j+1)*2]) ? j*2+1 : (j+1)*2;
+					}
+					if(A[j] > A[min]){
+						swap(A+j, A+min);
+						j = min;	
+					}
+					else{
+						break;
+					}
+				}
+			}
+			i--;
+		}
+		sort[x++] = A[0];
+		swap(A, A+n-1);
+		n--;
+	}
+	return sort;
+}
+
+int main(){
+	int heap[13] = {2, 5, 5, 8, 9, 4, 3, 7, 1, 10, 26, 39, 14};
+	int* sort = (int*)malloc(sizeof(int)*13);
+	sort = heapsort(heap, 13);
+	for(int i = 0; i < 13; i++){
+		printf("%d ", sort[i]);
+	} 
+}
